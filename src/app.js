eae2cdd327b6ad4b512241c0f8193b13d0941ca0
Cafuxx <millo1234lol@gmail.com>
2024-04-14 20:49:27 -0300

    creacion del archivo .env y configuracion de la conexion con la bbdd

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
-
+// Routes
+import personaRoutes from "./routes/persona.routes";
 const app=express();
 
 // Settings
@@ -8,5 +9,8 @@ app.set("port", 4000);
 
 // Middlewares
 app.use(morgan("dev"));
+
+// Routes
+app.use("/api/persona", personaRoutes);
 
 export default app;
--- src/routes/persona.routes.js
+++ src/routes/persona.routes.js
@@ -1,9 +1,7 @@
 import { Router } from "express";
-
+import { methods as personaController} from "../controllers/persona.controller";
 const router=Router();
 
-router.get("/",(request, response) => {
-    res.send("facu puto");
-});
+router.get("/", personaController.getPersona);
 
 export default router;
