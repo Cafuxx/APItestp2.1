311b5e3ff57a2ba9b48b31133938844a8324aa71
Cafuxx <millo1234lol@gmail.com>
2024-04-15 09:21:13 -0300

    implementacion del endpoint para listar personas por su id

--- src/controllers/persona.controller.js
+++ src/controllers/persona.controller.js
@@ -1,6 +1,6 @@
 import { getConnection } from "./../database/database";
 
-const getPersona = async (req, res) => {
+const getPersonas = async (req, res) => {
     try{
         const connection = await getConnection();
         const result = await connection.query("SELECT id, Nombre, Dni, FechaNacimiento FROM persona");
@@ -12,6 +12,43 @@ const getPersona = async (req, res) => {
     
 };
 
+const getPersona = async (req, res) => {
+    try{
+        console.log(req.params);
+        const { id } = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id, Nombre, Dni, FechaNacimiento FROM persona WHERE id = ?", id);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+    
+};
+
+const addPersona = async (req, res) => {
+    try{
+        const { nombre, dni, fechanacimiento} = req.body;
+        
+        if(nombre===undefined || dni===undefined || fechanacimiento===undefined){
+            res.status(400).json({message:"Bad request. Please fill all the fields."});
+        }
+
+        const persona={ nombre, dni, fechanacimiento };
+        console.log(req.body);
+        const result = await connection.query("INSERT INTO persona SET ?", persona); 
+        const connection = await getConnection();
+        
+        res.json({message: "Datos añadidos"});
+    }catch(error){
+        res.status(500);
+        res.send(error.message);
+    }
+    
+};
+
 export const methods = {
-    getPersona
+    getPersonas,
+    getPersona,
+    addPersona
 };
--- src/routes/persona.routes.js
+++ src/routes/persona.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { methods as personaController} from "../controllers/persona.controller";
+import { methods as personaController } from "../controllers/persona.controller";
 const router=Router();
 
 router.get("/", personaController.getPersona);
+router.post("/", personaController.addPersona);
 
 export default router;
